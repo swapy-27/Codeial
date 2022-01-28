b3611e21112493d0ab08cc4ebfe42165251aad95
Swapnil <swapniljain240@gmail.com>
2022-01-29 01:56:27 +0530

    adding more controllers

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,20 @@ const homeController = require('../controllers/home_controller');
 console.log('Router is Loaded')
 router.get('/',homeController.home);
 
+//for any other further routes acess from here 
+// router.use('/routerName',require('./routerfile));
+
+router.use('/user',require('./user'));
+
+router.use('/post',require('./post'));
+
+
+
+
+
+
+
+
+
+
 module.exports=router;
