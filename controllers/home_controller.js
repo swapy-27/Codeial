954f67e1b1cb389afbf365d7dc59145bb97b01ea
Swapnil <swapniljain240@gmail.com>
2022-01-29 00:50:46 +0530

    add a controller and Router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = function(req , res){
+    return res.end('<h1>Home is up for action</h1>')
+}
+
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require ('express');
 const app = express();
-
 const port = 8000;
+
+//use express Router
+app.use('/',require('./routes'));
 
 app.listen(port,function(err){
     if(err){
