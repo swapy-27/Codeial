35d287034c53e7bf81173f81cf510dc240b636a1
Swapnil <swapniljain240@gmail.com>
2022-01-30 18:50:00 +0530

    manual authentication

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,9 +1,19 @@
 const User = require('../models/user');
 
 module.exports.profile = function (req, res) {
-    res.render('user_profile', {
-        title: "Profile"
-    })
+    var user_id=req.cookies.user_id
+    if (user_id){
+        User.findById(user_id,function(err,user){
+            res.render('user_profile', {
+                title: "Profile",
+                 email:user.email,
+                 name:user.name
+            })
+        })
+    }else{
+        return  res.redirect('/user/sign-in');
+    }
+
 }
 module.exports.signIn = function (req, res) {
     res.render('user_sign_In', {
@@ -46,7 +56,44 @@ module.exports.create = function (req, res) {
     })
 }
 
+//sign In and create session for user
 module.exports.createSession = function (req, res) {
+    //find the user
+    User.findOne({email:req.body.email},function(err,user){
+
+        if (err){
+            console.log('error finding user while signing in');
+            return;
+        }
+        //handle user found
+        if (user){
+             //handle password ehich don't match
+            if (req.body.password!=user.password){
+                return res.redirect('back');
+            }
+
+            res.cookie('user_id',user._id);
+
+            return res.redirect('/user/profile');
+             //handle session creation
+
+        }
+        else{
+            //handle user not found
+            return res.redirect('back');
+        }
+    })
+
 
 }
 
+module.exports.signOut = function(req,res){
+    console.log(req.body);
+    if (req.cookies.user_id){
+        res.clearCookie('user_Id');
+        return res.redirect('/user/sign-in');
+    }
+    else{
+        return res.redirect('back');
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/user_controller');
 
 
-router.get('/',userController.profile);
+router.get('/profile',userController.profile);
 router.get('/sign-in',userController.signIn);
 router.get('/sign-up',userController.signUp);
 
@@ -12,5 +12,7 @@ router.get('/sign-up',userController.signUp);
 router.post('/create',userController.create);
 router.post('/create-session',userController.createSession);
 
+
+router.get('/sign-out',userController.signOut);
 
 module.exports=router;
